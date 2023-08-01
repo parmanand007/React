797edce76d90c2a1255c30839754adae2e56d64c
parmanand007 <parmanand.p@brimo.in>
2023-08-02 00:16:55 +0530

    igniting our app

--- JS/app.js
+++
@@ -1,4 +0,0 @@
-const root=document.getElementById("root")
-const heading=document.createElement("h1")
-heading.innerText="Hello World"
-root.appendChild(heading)
---
+++ app.js
@@ -0,0 +1,14 @@
+import React from "react"
+import  ReactDOM  from "react-dom/client"
+
+const heading=React.createElement(
+    "h1",
+    {
+        key:"124"
+    }
+    ,"heading 1"
+)
+const container =React.createElement(
+    "div",{id:"container"},
+    heading
+)
